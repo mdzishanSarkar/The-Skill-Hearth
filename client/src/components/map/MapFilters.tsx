1c4cf900ba49e7ca82045cf9f1f8519236b64238
mdzishanSarkar <mdzishansarkarmurad@gmail.com>
2026-09-16 12:26:31 +0600

    Updated

--- client/src/components/map/MapFilters.tsx
+++ client/src/components/map/MapFilters.tsx
@@ -113,7 +113,7 @@ export default function MapFilters({
           id="map-radius"
           type="range"
           min={1}
-          max={20}
+          max={100}
           step={1}
           value={radiusKm}
           onChange={(e) => onRadiusChange(Number(e.target.value))}
